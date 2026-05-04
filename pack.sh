82cbb0f6b97c4c5b4ef5ab407d6b8b14639a84d6
ShiralynDev <jultomtenriktig@gmail.com>
2026-05-04 04:56:14 +0200

    avoid copying folder twice & zip function in pack script

--- pack.sh
+++ pack.sh
@@ -1,8 +1,18 @@
 mkdir ./DiscordRPContinued
 mkdir ./DiscordRPContinued/Plugins
 cp ./DiscordRP/bin/Release/DiscordRP.dll ./DiscordRPContinued/Plugins/DiscordRP.dll
-cp -r ./DiscordRP/Libraries/linux ./DiscordRPContinued/Plugins/linux
-cp -r ./DiscordRP/Libraries/osx ./DiscordRPContinued/Plugins/osx
-cp -r ./DiscordRP/Libraries/win32 ./DiscordRPContinued/Plugins/win32
-cp -r ./DiscordRP/Libraries/win64 ./DiscordRPContinued/Plugins/win64
+cp -r ./DiscordRP/Libraries/linux ./DiscordRPContinued/Plugins/
+cp -r ./DiscordRP/Libraries/osx ./DiscordRPContinued/Plugins/
+cp -r ./DiscordRP/Libraries/win32 ./DiscordRPContinued/Plugins/
+cp -r ./DiscordRP/Libraries/win64 ./DiscordRPContinued/Plugins/
 cp ./LICENSE ./DiscordRPContinued/LICENSE
+
+if [ $# -eq 1 ]; then
+    if [ $1 == "help" ]; then
+        echo "help, zip"
+    fi
+
+    if [ $1 == "zip" ]; then
+        zip -r './DiscordRPContinued.zip' './DiscordRPContinued'
+    fi
+fi
